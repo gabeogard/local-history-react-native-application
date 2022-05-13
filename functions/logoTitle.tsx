bada0fa72f75d87c5c4078778ccbea42fd2245a3
mojibhashemi123 <muha006@student.kristiania.no>
2022-05-13 21:59:41 +0200

    Refactoring, move LogoTitle to function directory

---
+++ functions/logoTitle.tsx
@@ -0,0 +1,77 @@
+import {Image, ImageBackground, StyleSheet, View} from "react-native";
+import * as React from "react";
+
+export function LogoTitle() {
+    return (
+        <View style={styles.container}>
+            <ImageBackground
+                style={StyleSheet.absoluteFillObject}
+
+                source={require("../res/images/header.png")}
+                imageStyle={styles.container}
+            />
+            <Image style={styles.sun} source={require("../res/images/sun.png")}/>
+
+            <View style={styles.circleImageWrapper}>
+                <Image style={styles.circle} source={require("../res/images/logo-circle.png")}/>
+            </View>
+
+            <Image style={styles.logo} source={require("../res/images/kommune-logo.png")}/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+        container: {
+            backgroundColor: "#FBF4E6",
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            borderColor: "#000000",
+            borderBottomWidth: 1,
+
+            shadowColor: "#000000",
+            shadowOffset: {width: 0, height: 4},
+            shadowOpacity: 0.3,
+            shadowRadius: 4,
+        },
+        backgroundImage: {
+            height: 60,
+
+        },
+        circleImageWrapper: {
+            height: 80,
+            width: 90,
+            top: 21,
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 5,
+            },
+            shadowOpacity: 0.36,
+            shadowRadius: 6.68,
+
+            elevation: 11,
+        },
+
+        circle: {
+            width: "100%",
+            height: "100%",
+            borderRadius: 100,
+        },
+        sun: {
+            height: 40,
+            width: 40,
+            top: 40,
+            left: 15
+        },
+        logo: {
+            height: 25,
+            width: 70,
+            zIndex: 1,
+            top: -35
+        }
+    }
+)
+
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,23 +3,23 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import {FontAwesome} from '@expo/vector-icons';
+import {AntDesign, Entypo} from '@expo/vector-icons';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {DarkTheme, DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-import {ColorSchemeName, Image, ImageBackground, SafeAreaView, StyleSheet, View} from 'react-native';
-import { AntDesign,Entypo } from '@expo/vector-icons';
+import {ColorSchemeName, Image, SafeAreaView, View} from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import Home from '../screens/Home';
 import TabTwoScreen from '../screens/TabTwoScreen';
-import {RootStackParamList, RootTabParamList, RootTabScreenProps} from '../types';
+import {RootStackParamList} from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 import {LoginScreen} from "../screens/LoginScreen";
 import {FactsScreen} from "../screens/Facts";
+import {LogoTitle} from "../functions/logoTitle";
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
@@ -53,80 +53,6 @@ function RootNavigator() {
 
 const BottomTab = createBottomTabNavigator();
 
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#FBF4E6",
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        borderBottomColor: '#000000',
-        borderBottomWidth: 1,
-
-        shadowColor: "#000000",
-        shadowOffset: {width: 0, height: 4},
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-    },
-    backgroundImage: {
-        height:60,
-
-    },
-    circleImageWrapper: {
-        height: 80,
-        width: 90,
-        top: 21,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.36,
-        shadowRadius: 6.68,
-
-        elevation: 11,
-    },
-
-     circle: {
-         width: "100%",
-         height: "100%",
-         borderRadius: 100,
-    },
-    sun: {
-        height: 40,
-        width: 40,
-        top: 40,
-        left: 15
-    },
-    logo: {
-       height: 25,
-       width: 70,
-       zIndex:1,
-       top: -35
-    }
-}
-  )
-
-function LogoTitle() {
-    return (
-        <View style={styles.container}>
-        <ImageBackground
-            style={StyleSheet.absoluteFillObject}
-
-            source={require("../res/images/header.png")}
-            imageStyle={styles.container}
-        />
-            <Image style={styles.sun} source={require("../res/images/sun.png")} />
-
-            <View style={styles.circleImageWrapper}>
-            <Image style={styles.circle} source={require("../res/images/logo-circle.png")} />
-            </View>
-
-            <Image style={styles.logo} source={require("../res/images/kommune-logo.png")} />
-        </View>
-    );
-}
 
 function Title () {
     return (
