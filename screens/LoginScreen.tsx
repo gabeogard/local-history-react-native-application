524bb8dd0aefb4b67d41fbe4f59125c72ffb0c9d
mojibhashemi123 <muha006@student.kristiania.no>
2022-05-20 03:49:22 +0200

    login almost done

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,85 +1,158 @@
-import {Text, View} from "../components/Themed";
-import EditScreenInfo from "../components/EditScreenInfo";
+import React, {useEffect, useState} from "react"
 import {
+    View,
+    Text,
     StyleSheet,
     TextInput,
-    Image,
-    ImageRequireSource,
-    SafeAreaView,
-    StatusBar,
     TouchableOpacity,
-    Platform
+    KeyboardAvoidingView, Dimensions, ImageBackground
 } from "react-native";
-import {useState} from "react";
+import {auth} from "../firebase.js";
+import {signInWithEmailAndPassword, onAuthStateChanged} from "firebase/auth";
 
-
-export function LoginScreen() {
-        const [userName, setUserName] = useState("");
+export function LoginScreen({navigation}:{navigation: any}) {
+        const [email, setEmail] = useState("");
         const [password, setPassword] = useState("");
 
+        //bruk denne
+        useEffect(() => {
+            const unsubscribe = auth
+                .onAuthStateChanged((user: any) => {
+                if (user) {
+                    navigation.navigate("userProfile")
+                }
+            })
+
+            return unsubscribe
+        }, [])
+
+        const onPressLogin = async () => {
+            try {
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+                console.log(userCredential )
+
+            } catch (error) {
+                alert(error.message)
+            }
+        }
+
     return (
-        <View style={styles.container}>
-            <Image style={styles.image}  source={require("../res/images/landing-picture.png")} />
-            <View style={styles.inputView}>
+        <KeyboardAvoidingView style={styles.container} behavior={"padding"}>
+
+            <ImageBackground style={styles.introBox} source={require("../res/images/landing-picture.png")}>
+                <Text style={styles.textOnBackground}>Byåa Kultursti</Text>
+            </ImageBackground>
+
+            <Text></Text>
+
+            <View style={styles.inputContainer}>
                 <TextInput
-                    style={styles.TextInput}
-                    placeholder="Brukernavn"
-                    placeholderTextColor="#003f5c"
-                    onChangeText={(username) => setUserName(username)}
+                style={styles.input}
+                placeholder={"Epost"}
+                value={email}
+                onChangeText={text => setEmail(text)}
+                returnKeyType={ "done" }
+                autoCapitalize="none"
                 />
-            </View>
 
-            <View style={styles.inputView}>
                 <TextInput
-                    style={styles.TextInput}
-                    placeholder="Passord."
-                    placeholderTextColor="#003f5c"
-                    secureTextEntry={true}
-                    onChangeText={(password) => setPassword(password)}
+                    style={styles.input}
+                    placeholder={"Passord"}
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    returnKeyType={ "done" }
+                    secureTextEntry
                 />
+
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                        onPress={() => {onPressLogin()}}
+                        style={styles.button}
+                    >
+                        <Text style={styles.text}>Login</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        style={styles.button}
+                    >
+                        <Text style={styles.text}>Register</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
+        </KeyboardAvoidingView>
 
-            <TouchableOpacity style={styles.loginBtn}>
-                <Text>LOGIN</Text>
-            </TouchableOpacity>
-
-
-        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: '#FBF4E6',
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    image: {
-        marginTop: Platform.OS === "web" ? 0 : 20,
-        height: Platform.OS === "web" ? 115 :  89,
-        width: Platform.OS === "web" ? 350 : 278,
-    },
-    inputView: {
-        backgroundColor: "#FFFFFF",
-        borderRadius: 30,
-        height: 45,
-        marginBottom: 20,
+        backgroundColor: "#FBF4E6",
+        justifyContent: "center",
         alignItems: "center",
+        paddingHorizontal: 20,
     },
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
+    introBox: {
+        width: Dimensions.get("window").width >= 375 ? 350: 278,
+        height: Dimensions.get("window").width >= 375 ? 115: 90,
+        top: -50,
+        display: "flex",
+        alignContent: "center",
+        justifyContent: "center",
+
+        shadowColor: "#000000",
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.5,
+        shadowRadius: 4,
     },
-    loginBtn: {
+    textOnBackground: {
+        //fontFamily: 'Roboto-Regular',
+        fontStyle: "normal",
+        fontWeight: "400",
+        fontSize: 24,
+        lineHeight: 28,
+        paddingLeft: 32,
+        paddingBottom: 33,
+
+        shadowColor: "#000000",
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.5,
+        shadowRadius: 4,
+    },
+    inputContainer: {
+        backgroundColor: "#FBF4E6",
+        top: -20,
+    },
+    input: {
+        height: 35,
+        width: 200,
+        borderRadius: 12,
+        backgroundColor: "#fff",
+        borderWidth:1,
+        marginTop: 12,
+        padding: 9,
+        top: -20
+    },
+    buttonContainer:{
+        backgroundColor: "#FBF4E6",
+        top: Dimensions.get("window").width >= 375 ? 5: 0,
+        flexDirection: "row",
+        justifyContent: "space-evenly"
+    },
+    button: {
         backgroundColor: "#F5BFB6",
-        marginTop: Platform.OS === "web" ? 5 : 4,
+        marginTop: Dimensions.get("window").width >= 375 ? 10: 5,
         borderWidth:1,
         borderRadius:6,
+        padding: 3,
+        shadowColor: "#000000",
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
     },
-});
+    text: {
+        fontSize: 14,
+    }
+})
