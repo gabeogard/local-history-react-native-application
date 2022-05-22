2548bb36ff472eaa56ae5e2662f9900ef5b24633
mojibhashemi123 <muha006@student.kristiania.no>
2022-05-23 01:21:10 +0200

    userProfile showing username

--- screens/UserProfileScreen.tsx
+++ screens/UserProfileScreen.tsx
@@ -1,12 +1,54 @@
 import {SafeAreaView, Text, View} from "react-native";
+import {db} from "../firebase";
+import {collection, getDocs} from "firebase/firestore/lite"
+import {useEffect, useState} from "react";
+
 
 export function UserProfileScreen() {
+
+    const [users, setUsers] = useState([])
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const usersCollectionRef = collection(db, "users")
+
+    useEffect(() => {
+        setIsLoading(true)
+        const getUsers = async () => {
+
+           try {
+               let data = await getDocs((usersCollectionRef as any))
+               setUsers(data.docs.map((doc) => ({...doc.data() as any, id: doc.id})) as any)
+               console.log(data)
+               setIsLoading(false)
+
+           } catch (error){
+               console.log(error)
+           }
+        }
+
+        getUsers()
+
+    }, [])
+
+    if (isLoading){
+        return (
+            <View style={{flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#fff"}}><Text>Loading...</Text></View>
+        )
+    }
+
     return (
 
         <SafeAreaView style={{flex: 1, backgroundColor: "#fff"}} >
             <View style={{flex: 1}} >
+
+                {users.map((users, index) => {
+                    return <Text key={index}>name: {(users as any)?.username}</Text>
+                })}
+
                 <Text>Du er logget in</Text>
             </View>
         </SafeAreaView>
     );
+
 }
