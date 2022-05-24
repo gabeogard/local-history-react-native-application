aec0b2e0b175ce83d003d1f2205bd109981bf76b
mojibhashemi123 <muha006@student.kristiania.no>
2022-05-24 02:57:49 +0200

    almost done with UserProfile

--- screens/UserProfileScreen.tsx
+++ screens/UserProfileScreen.tsx
@@ -1,4 +1,4 @@
-import {Image, SafeAreaView, StyleSheet, Text, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
 import {auth, db} from "../firebase";
 import {collection, getDocs, query} from "firebase/firestore/lite"
 import {useEffect, useState} from "react";
@@ -47,25 +47,75 @@ export function UserProfileScreen() {
     return (
 
         <View style={styles.container}>
-            <View style={styles.profileHeader}>
+
+            <View style={[styles.profileHeader, styles.shadow]}>
 
                 <View style={{justifyContent: "flex-end"}}>
-                <Image style={{width: 150, height: 150, tintColor: "#3F474C" }}
+                    <Image style={{width: 150, height: 150, tintColor: "#3F474C" }}
                        source={require("../res/images/profile/icon-account.png")}/>
                 </View>
 
-                <View style={{justifyContent: "center"}}>
+                <View style={{flex: 1, justifyContent: "center"}}>
                     <View style={{alignItems: "center"}}>
-                    <Text style={{justifyContent: "center"}}>Info: </Text>
+                    <Text style={{justifyContent: "center", fontWeight: "bold", color: "#3F474C"}}>Info: </Text>
                     </View>
-                    <View style={{borderTopWidth: 1, marginTop: 5, marginBottom: 5}}></View>
-                    <Text>{auth.currentUser?.email}</Text>
-                    <Text>Bruker: {Object.values(username)}</Text>
+
+                    <View style={{borderTopWidth: 1, marginTop: 5, marginBottom: 5, borderColor: "#3F474C"}}></View>
+
+                    <View style={{width: "97%"}}>
+                        <Text numberOfLines={1} adjustsFontSizeToFit style={styles.text}>{auth.currentUser?.email}</Text>
+                        <Text numberOfLines={1} adjustsFontSizeToFit style={styles.text}>Bruker: {Object.values(username)}</Text>
+                    </View>
                 </View>
 
             </View>
-                <View style={{flex: 1}} >
-                <Text style={{top: "20%"}}>Du er logget på, brukerN: {Object.values(username)}</Text>
+
+            <View style={{flex: 1, flexDirection: "row", width: "100%", marginTop: "10%", marginBottom: "15%" }}>
+
+            <View style={{flex: 1, alignItems:"center", justifyContent: "space-evenly"}}>
+
+                <View style={{backgroundColor: "#8DC6C2", width: "92%", height: "15%", justifyContent: "center", alignItems: "center"}}>
+                    <Text>Status:</Text>
+                </View>
+                <View style={{backgroundColor: "#8DC6C2", width: "92%", height: "15%", justifyContent: "center", alignItems: "center"}}>
+                    <Text>Bio:</Text>
+                </View>
+                <View style={{backgroundColor: "#8DC6C2", width: "92%", height: "15%", justifyContent: "center", alignItems: "center"}}>
+                    <Text>Quiz resultat:</Text>
+                </View>
+
+            </View>
+
+                <View style={{flex: 2, alignItems:"center", justifyContent: "space-evenly"}}>
+
+
+                    <View style={{width: "95%", height: "15%"}}>
+                        <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between"}}>
+                        <View style={{backgroundColor: "#3F474C", width: "30%", height: "100%", justifyContent: "center", alignItems: "center"}}>
+                            <Text style={styles.textFlex}>Online</Text>
+                        </View>
+                        <View style={{backgroundColor: "#3F474C", width: "68%", height: "100%", justifyContent: "center", alignItems: "center"}}>
+                            <Text style={styles.textFlex}>Date/opprettet</Text>
+                        </View>
+                        </View>
+                    </View>
+
+
+                    <View style={{backgroundColor: "#3F474C", width: "95%", height: "15%", justifyContent: "center", alignItems: "center"}}>
+                        <Text style={styles.textFlex}>Her kan vi bestemme</Text>
+                    </View>
+
+                    <View style={{width: "95%", height: "15%"}}>
+                        <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between"}}>
+                            <View style={{backgroundColor: "#3F474C", width: "70%", height: "100%", justifyContent: "center", alignItems: "center"}}>
+                                <Text style={styles.textFlex}>Din score er</Text>
+                            </View>
+                            <View style={{backgroundColor: "#3F474C", width: "28%", height: "100%", justifyContent: "center", alignItems: "center"}}>
+                                <Text style={styles.textFlex}>30</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
 
             </View>
 
@@ -92,6 +142,18 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "40%",
         borderBottomEndRadius: 100,
-
+    },
+    text: {
+        marginBottom: 2,
+        color: "#3F474C"
+    },
+    textFlex: {
+        color: "white"
+    },
+    shadow: {
+        shadowColor: "#000000",
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.4,
+        shadowRadius: 10,
     }
 })
