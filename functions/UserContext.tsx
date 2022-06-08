20a5bb8747b6f6feecb0e52a64f86beb3b1e3f97
mojibhashemi123 <muha006@student.kristiania.no>
2022-06-08 22:41:15 +0200

    Error handler

--- functions/UserContext.tsx
+++ functions/UserContext.tsx
@@ -66,20 +66,7 @@ export const UserContextProvider = ({
                 }
             )
         } catch (error) {
-            if (error.message === "Firebase: Error (auth/invalid-email).") {
-                Alert.alert("Ugyldig epost", "Prøv igjen");
-            } else if (error.message === "Firebase: Error (auth/wrong-password).") {
-                Alert.alert("Ugyldig password", "Prøv igjen");
-            }else if (error.message === "Firebase: Error (auth/user-not-found).") {
-                Alert.alert("Finner ikke bruker.", " Vennligst registrer deg.")
-            } else if (error.message === "Firebase: Error (auth/missing-email).") {
-                Alert.alert("Du mangler Epost.", "Skriv inn Epost")
-            }else if (error.message === "Firebase: Password should be at least 6 characters (auth/weak-password).") {
-                Alert.alert("Du har valgt et svakt passord.", "Du må minimum ha 6 tegn.")
-            }
-            else {
-                alert(error.message);
-            }
+            ErrorHandler(error)
         } finally {
             setLoading(false)
         }
@@ -114,7 +101,7 @@ export const UserContextProvider = ({
                 'Du har fått en melding på din e-postadresse'
             )
         } catch (error) {
-            alert(error.message)
+            ErrorHandler(error)
         } finally {
             setLoading(false)
         }
